979814d30986095599ca0015692d24a5d93d237d
lsc <lisc0788@aresoft.cn>
2020-08-19 17:34:44 +0800

    t commit -m 'first commit'# Changes to be committed:

---
+++ src/main/java/cn/aresoft/controller/TestController.java
@@ -0,0 +1,14 @@
+package cn.aresoft.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("test")
+@RestController
+public class TestController {
+
+     @RequestMapping("test")
+     public String test(){
+        return "Hello World";
+     }
+}
